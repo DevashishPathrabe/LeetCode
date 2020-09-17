7144379e3b3409461b3e73a5da5665f965470470
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-09-18 01:47:10 +0530

    Check If N and Its Double Exist
    Here is my Check If N and Its Double Exist LeetCode Solution...

---
+++ LeetCode Solutions/Arrays 101/Check If N and Its Double Exist.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        hashSet = set()
+        for i in range(len(arr)):
+            if((arr[i]*2 in hashSet) or (arr[i]%2 == 0 and arr[i]//2 in hashSet)):
+                return True
+            hashSet.add(arr[i])
+        return False

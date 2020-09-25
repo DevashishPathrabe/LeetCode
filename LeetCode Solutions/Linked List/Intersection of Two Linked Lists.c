6f6aa6337cc17265ed9bf867ffc8829a73d2947c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-09-25 20:20:36 +0530

    Intersection of Two Linked Lists
    Here is my Intersection of Two Linked Lists LeetCode Solution...

---
+++ LeetCode Solutions/Linked List/Intersection of Two Linked Lists.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    struct ListNode *head1 = headA;
+    struct ListNode *head2 = headB;
+    while(head1 != head2){
+        if(head1 == NULL){
+            head1 = headB;
+        } else{
+            head1 = head1->next;
+        }
+        if(head2 == NULL){
+            head2 = headA;
+        } else{
+            head2 = head2->next;
+        }
+    }
+    return head1;
+}

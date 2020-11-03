6b77a4558b7c84c4e2d04ec69f3125594ed5b314
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-03 19:51:12 +0530

    Consecutive Characters
    Here is my Consecutive Characters LeetCode Solution...

---
+++ LeetCode Solutions/November LeetCoding Challenge/Consecutive Characters.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxPower(String s) {
+        int count = 1;
+        int power = 1;
+        for(int i=0; i<s.length() - 1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                count++;
+            } else{
+                count = 1;
+            }
+            power = Math.max(power, count);
+        }
+        return power;
+    }
+}

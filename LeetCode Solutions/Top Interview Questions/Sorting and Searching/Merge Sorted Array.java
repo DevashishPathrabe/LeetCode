2d28605cdcd53d66be2b5c917241e30bc9cf4f64
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-09 22:38:17 +0530

    Delete Merge Sorted Array.java

--- LeetCode Solutions/Top Interview Questions/Sorting and Searching/Merge Sorted Array.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0; i<n; i++){
-            nums1[m] = nums2[i];
-            m += 1;
-        }
-        Arrays.sort(nums1);
-    }
-}

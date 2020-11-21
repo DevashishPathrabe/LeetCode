58e4017bb939d646722a5d34cd6e9bac7be38a95
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-21 21:04:01 +0530

    N-ary Tree Postorder Traversal
    Here is my N-ary Tree Postorder Traversal LeetCode Solution...

---
+++ LeetCode Solutions/N-ary Tree/N-ary Tree Postorder Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if(not root):
+            return []
+        else:
+            resultTree = []
+            for child in root.children:
+                resultTree += self.postorder(child)
+            resultTree.append(root.val)
+        return resultTree

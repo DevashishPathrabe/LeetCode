ba9575fd4295ede9f03b2a5c5c44284a2d9f97c9
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-24 14:05:20 +0530

    Reverse Integer
    Here is my Reverse Integer LeetCode Solution...

---
+++ LeetCode Solutions/Top Interview Questions(Easy Collection)/Strings/Reverse Integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        signed = (x > 0) - (x < 0)
+        reverse = int(str(x*signed)[::-1])
+        if(signed*reverse < -2**31 or signed*reverse > 2**31 - 1):
+            return 0
+        else:
+            return signed * reverse

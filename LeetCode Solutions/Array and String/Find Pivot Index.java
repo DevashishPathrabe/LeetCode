a070cecd94f394136def34866d8187dc21896590
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-12-04 23:21:15 +0530

    Find Pivot Index
    Here is my Find Pivot Index LeetCode Solution...

---
+++ LeetCode Solutions/Array and String/Find Pivot Index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        if(nums.length < 1){
+            return -1;
+        }
+        int rightSum=0, leftSum=0;
+        for(int i:nums){
+            rightSum += i;
+        }
+        for(int i=0; i<nums.length; i++){
+            rightSum -= nums[i];
+            if(rightSum == leftSum)
+                return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}

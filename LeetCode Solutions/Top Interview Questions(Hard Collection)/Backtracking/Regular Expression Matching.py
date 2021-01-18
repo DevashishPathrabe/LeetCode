ac6b73726138fbbcc06bd49dde66482aa3e3b6b2
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-01-18 23:32:32 +0530

    Regular Expression Matching
    Here is my Regular Expression Matching LeetCode Solution...

---
+++ LeetCode Solutions/Top Interview Questions(Hard Collection)/Backtracking/Regular Expression Matching.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        match = re.match(p,s)
+        if(not match):
+            return False
+        if(match.start() == 0 and match.end() == len(s)):
+            return True
+        return False

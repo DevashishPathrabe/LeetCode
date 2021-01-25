9a83e8f0c33f738fcb4702a8211f36f341e63930
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-01-26 01:05:28 +0530

    Check If All 1's Are at Least Length K Places Away
    Here is my Check If All 1's Are at Least Length K Places Away LeetCode Solution...

---
+++ LeetCode Solutions/January LeetCoding Challenge/Check If All 1's Are at Least Length K Places Away.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        count = 0;
+        for i in range(len(nums)):
+            if(nums[i] == 1 and count < k and i != 0):
+                return False
+            elif(nums[i] == 1):
+                count = 0
+            else:
+                count += 1
+        return True

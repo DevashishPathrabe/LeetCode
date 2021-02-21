2fa7f38914ccbd7a13b8dc410e60ca20c41f753c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-02-21 23:54:40 +0530

    Broken Calculator
    Here is my Broken Calculator LeetCode Solution...

---
+++ LeetCode Solutions/February LeetCoding Challenge/Broken Calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int result = 0;
+        while(X < Y){
+            result++;
+            if(Y%2 > 0){
+                Y++;
+            } else{
+                Y /= 2;
+            }
+        }
+        return result + X - Y;
+    }
+};

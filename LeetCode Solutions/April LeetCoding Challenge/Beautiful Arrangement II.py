f4906d052da866635ca28e8efd628ae2baebe207
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-04-13 00:57:14 +0530

    Beautiful Arrangement II
    Here is my Beautiful Arrangement II LeetCode Solution...

---
+++ LeetCode Solutions/April LeetCoding Challenge/Beautiful Arrangement II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def constructArray(self, n: int, k: int) -> List[int]:
+        result, a, z = [0]*n, 1, k+1
+        for i in range(k+1):
+            if(i%2):
+                result[i] = z
+                z -= 1
+            else:
+                result[i] = a
+                a += 1
+        for i in range(k+1, n):
+            result[i] = i+1
+        return result

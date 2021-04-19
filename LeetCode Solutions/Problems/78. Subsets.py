f99e7bbd416c0f4d4a0a166309ef4beea39ae35b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-04-19 21:40:00 +0530

    78. Subsets
    Here is my Subsets LeetCode Solution...

---
+++ LeetCode Solutions/Problems/78. Subsets.py
@@ -0,0 +1,6 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if(len(nums) == 0):
+            return [[]]
+        sets = self.subsets(nums[1:])
+        return sets + [[nums[0]] + s for s in sets]

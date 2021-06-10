1350d6a36bf02268a63728631a6580ba3d4dd93e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-06-10 21:47:08 +0530

    970. Powerful Integers
    Here is my Powerful Integers LeetCode Solution...

---
+++ LeetCode Solutions/Problems/970. Powerful Integers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        listOfPowerfullIntegers, xi = set(), 1
+        while(xi < bound):
+            yj = 1
+            while(xi + yj <= bound):
+                listOfPowerfullIntegers.add(xi + yj)
+                if(y == 1):
+                    break
+                yj *= y
+            if(x == 1):
+                break
+            xi *= x
+        return list(listOfPowerfullIntegers)

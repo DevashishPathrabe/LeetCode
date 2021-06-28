c4f1ce4193b1ad187b5a3be589607971aafdc36c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-06-28 18:42:04 +0530

    290. Word Pattern
    Here is my Word Pattern LeetCode Solution...

---
+++ LeetCode Solutions/Problems/290. Word Pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        d = {}
+        word = s.split(" ")
+        if(len(word) != len(pattern)):
+            return False
+        for i in range(len(word)):
+            if(pattern[i] not in d):
+                if(word[i] not in d.values()):
+                    d[pattern[i]] = word[i]
+                else:
+                    return False
+            elif(d[pattern[i]] != word[i]):
+                return False
+        return True

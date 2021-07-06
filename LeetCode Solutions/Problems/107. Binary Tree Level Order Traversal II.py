6280bc7a15d9025dd6fd232646f55949686f21c8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-07-06 14:17:22 +0530

    107. Binary Tree Level Order Traversal II
    Here is my Binary Tree Level Order Traversal II LeetCode Solution...

---
+++ LeetCode Solutions/Problems/107. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return
+        q = []
+        q.append(root)
+        result = []
+        while q:
+            temp=[]
+            for _ in range(len(q)):
+                node = q.pop(0)
+                temp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if temp:
+                result.append(temp)
+        return result[::-1]

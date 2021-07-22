6072fb0ba0a557ebc8a01c28be6e9d303129c6d8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-07-22 19:31:33 +0530

    96. Unique Binary Search Trees
    Here is my Unique Binary Search Trees LeetCode Solution...

---
+++ LeetCode Solutions/Problems/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            int temp = 0;
+            for(int j=0; j<=i-1; j++){
+                temp += (dp[j]*dp[i-1-j]);
+            }
+            dp[i] = temp;
+        }
+        return dp[n];
+    }
+};

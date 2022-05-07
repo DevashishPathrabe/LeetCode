0a10af3e01c867e471bcb516ce72cc2d2efde6bd
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-05-07 11:20:35 +0530

    Remove Element

---
+++ LeetCode Solutions/Array and String/Remove Element/Remove Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int begin = 0, end = nums.length - 1;
+        while (begin <= end){
+            if (nums[begin] == val){
+                nums[begin] = nums[end];
+                end--;
+            } else{
+                begin++;
+            }
+        }
+        return begin;
+    }
+}

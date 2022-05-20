d3d0f547995eeac5fde0aca377c2c9c2733d2c2d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-05-20 11:33:14 +0530

    Find Minimum in Rotated Sorted Array II

---
+++ LeetCode Solutions/Binary Search/Find Minimum in Rotated Sorted Array II/Find Minimum in Rotated Sorted Array II.c
@@ -0,0 +1,17 @@
+
+
+int findMin(int* nums, int numsSize){
+    int left = 0, right = nums.size()-1;
+    while (left < right){
+        int mid = (left + right) / 2;
+        if (nums[mid] < nums[right]){
+            right = mid;
+        }
+        else if (nums[mid] > nums[right]){
+            left = mid + 1;
+        } else{  
+            right--;
+        }
+    }
+    return nums[left];
+}

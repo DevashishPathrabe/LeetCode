faadbc0f51a8bbe8b8a6fa2f9560b71579281400
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-05-25 10:35:07 +0530

    Increasing Triplet Subsequence

---
+++ LeetCode Solutions/December 2020 LeetCoding Challenge/Increasing Triplet Subsequence/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int left = INT_MAX, mid = INT_MAX;
+        for (int i : nums){
+            if (i <= left){
+                left = i;
+            }
+            else if (i <= mid){
+                mid = i;
+            } else{
+                return true;
+            }
+        }
+        return false;
+    }
+};

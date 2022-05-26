a388d5f1a19cb766e94d932d9042f8895cf69415
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-05-26 09:34:32 +0530

    The kth Factor of n

---
+++ LeetCode Solutions/December 2020 LeetCoding Challenge/The kth Factor of n/The kth Factor of n.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        i, count = 1, 0
+        while (i <= n):
+            if (n%i == 0):
+                count += 1
+            if (count == k):
+                return i
+            i += 1
+        return -1

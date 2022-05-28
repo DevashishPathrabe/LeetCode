ca9b3b1dbad859a8f254df102e935ae2a54cfb0b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-05-28 12:51:41 +0530

    House Robber III

---
+++ LeetCode Solutions/December 2021 LeetCoding Challenge/House Robber III/House Robber III.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def dfs(node):
+            leftNode, leftChildren = dfs(node.left) if node.left else (0,0)
+            rightNode, rightChildren = dfs(node.right) if node.right else (0,0)
+            return node.val + leftChildren + rightChildren, max(leftNode + rightNode, leftNode + rightChildren, leftChildren + rightChildren, leftChildren + rightNode)
+        if (root):
+            return max(dfs(root))
+        else:
+            return 0

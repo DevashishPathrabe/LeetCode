f1b8cd2fd4b625243d67f0d256a9e0b45f84333b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-15 12:31:10 +0530

    Number of 1 Bits

---
+++ LeetCode Solutions/February LeetCoding Challenge/Number of 1 Bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        numberOf1bits = 0
+        while(n):
+            numberOf1bits += n%2
+            n = n>>1
+        return numberOf1bits

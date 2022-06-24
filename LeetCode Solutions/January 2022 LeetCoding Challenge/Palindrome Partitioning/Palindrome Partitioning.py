784c79b6d001f563bc2fcafe0851ebe22f93ecf8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-24 22:09:13 +0530

    Palindrome Partitioning

---
+++ LeetCode Solutions/January 2022 LeetCoding Challenge/Palindrome Partitioning/Palindrome Partitioning.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        palindromePartitioning = []
+        def helper(s, way):
+            if (not s):
+                return
+            if (len(s) == 1):
+                palindromePartitioning.append(way+[s])
+                return
+            for i in range(1, len(s)):
+                if (s[:i] == s[:i][::-1]):
+                    helper(s[i:], way+[s[:i]])
+            if (s == s[::-1]):
+                palindromePartitioning.append(way+[s])
+            return
+        helper(s, [])
+        return palindromePartitioning

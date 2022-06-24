fc36556aab999be9691f0c5df9c2eeef8b39b370
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-24 22:21:59 +0530

    Sum of Root To Leaf Binary Numbers

---
+++ LeetCode Solutions/January 2022 LeetCoding Challenge/Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    Sum = 0
+    SumString = []
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        if (not root):
+            return
+        self.SumString.append(str(root.val))
+        if (not root.left and not root .right):
+            self.Sum += int(''.join(self.SumString), 2)
+        self.sumRootToLeaf(root.left)
+        self.sumRootToLeaf(root.right)
+        self.SumString.pop(-1)
+        return self.Sum

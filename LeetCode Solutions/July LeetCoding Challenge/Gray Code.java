02f82f49553d533a57f06cae449b6c9032687572
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-26 20:25:10 +0530

    Gray Code

---
+++ LeetCode Solutions/July LeetCoding Challenge/Gray Code.java
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        long long int p = pow(2,n);
+        for(int i=0; i<p; i++){
+            result.push_back(i^(i/2));
+        }
+        return result;
+    }
+};

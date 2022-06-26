aaf4905d2fc30a8040b5eb5d36ac5226fbb289dd
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-26 09:35:44 +0530

    Partition Array into Disjoint Intervals

---
+++ LeetCode Solutions/July 2021 LeetCoding Challenge/Partition Array into Disjoint Intervals/Partition Array into Disjoint Intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def partitionDisjoint(self, nums: List[int]) -> int:
+        max = nums[0]
+        for i in range(len(nums)-1):
+            flag = 1
+            if (nums[i] > max):
+                max = nums[i]
+            for j in range(i+1, len(nums)):
+                if (nums[j] < max):
+                    flag = 0
+                    break
+            if (flag == 1):
+                return i+1
+        return 0;

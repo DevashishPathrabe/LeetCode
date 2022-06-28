89ce84716e52355461874b26f2c29bd5074e2e55
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-28 09:12:01 +0530

    Redundant Connection

---
+++ LeetCode Solutions/June 2021 LeetCoding Challenge/Redundant Connection/Redundant Connection.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        parent.resize(edges.size()+1);
+        for (int i=0; i<parent.size(); i++){
+            parent[i] = i;
+        }
+        for (auto& e : edges){
+            if (find(e[0]) == find(e[1])){
+                return e;
+            }
+            else{
+                uniun(e[0], e[1]);
+            }
+        }
+        return edges[0];
+    }
+private:
+    vector<int> parent;
+    int find(int x){
+        if (x != parent[x]) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    void uniun(int x, int y){
+        parent[find(y)] = find(x);
+    }
+};

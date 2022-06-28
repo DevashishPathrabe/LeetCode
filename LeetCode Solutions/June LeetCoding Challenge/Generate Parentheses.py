6fe706e6f13cc3e6264acab46e9bf76e1756ea51
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-28 09:21:18 +0530

    Generate Parentheses

---
+++ LeetCode Solutions/June LeetCoding Challenge/Generate Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        arr, m = [], 2 * n
+        def helper(position: int, Open: int, sequence: int) -> None:
+            if(position == m):
+                res = [0] * m
+                for i in range(m):
+                    res[i] = "(" if sequence & 1 << i else ")"
+                arr.append("".join(res))
+                return
+            if(Open):
+                helper(position+1, Open-1, sequence)
+            if(m - position > Open):
+                helper(position+1, Open+1, sequence | 1 << position)
+        helper(0, 0, 0)
+        return arr

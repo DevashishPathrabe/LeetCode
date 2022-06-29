652929208ae27260c73374ece4a9e932515b0c4a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-29 09:42:21 +0530

    Linked List Cycle II

---
+++ LeetCode Solutions/Linked List/Linked List Cycle II/Linked List Cycle II.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while (fast && fast->next){
+        fast = fast->next->next;
+        slow = slow->next;
+        if (slow == fast){
+            slow = head;
+            while (slow){
+                if (slow == fast){
+                    return slow;
+                }
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+    }
+    return NULL;
+}

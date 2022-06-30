3b53cead2efc8f215bd4ec1542b3623f89ffbdaf
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-06-30 09:23:49 +0530

    Broken Calculator

---
+++ LeetCode Solutions/March 2022 LeetCoding Challenges/Broken Calculator/Broken Calculator.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int result = 0;
+        while(startValue < target){
+            result++;
+            if(target % 2 > 0){
+                target++;
+            } else{
+                target /= 2;
+            }
+        }
+        return result + startValue - target;
+    }
+}

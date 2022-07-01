23361e7cfc5316938f22170119ef6f1cbb7ce26a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-01 09:47:11 +0530

    Binary Tree Cameras

---
+++ LeetCode Solutions/May 2021 LeetCoding Challenges/Binary Tree Cameras/Binary Tree Cameras.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) {
+        return helper(root) > 2 ? minNumberOfCameras + 1 : minNumberOfCameras;
+    }
+    int helper(TreeNode* node) {
+        if (!node){
+            return 0;
+        }
+        int value = helper(node->left) + helper(node->right);
+        if (value == 0){
+            return 3;
+        }
+        if (value < 3){
+            return 0;
+        }
+        minNumberOfCameras++;
+        return 1;
+    }
+private:
+    int minNumberOfCameras = 0;
+};

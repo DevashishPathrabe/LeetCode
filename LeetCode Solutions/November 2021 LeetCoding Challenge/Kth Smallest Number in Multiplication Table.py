4708b3f7788daaa8ec9a5a0946291e51f9fa43c0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-02 14:48:53 +0530

    Kth Smallest Number in Multiplication Table

---
+++ LeetCode Solutions/November 2021 LeetCoding Challenge/Kth Smallest Number in Multiplication Table.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        def count(x):
+            return sum(min(x//i, n) for i in range(1, m+1))
+        left, right, mid, smallestElement = 0, m*n, 0, 0
+        while left <= right:
+            mid = (left + right) >> 1
+            if count(mid) < k:
+                left = mid + 1
+            else:
+                right, smallestElement = mid - 1, mid
+        return smallestElement

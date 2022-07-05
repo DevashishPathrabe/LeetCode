74e3a237435ff1173ae4256cf487d7e6e90383f7
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-05 09:21:14 +0530

    1276. Number of Burgers with No Waste of Ingredients

---
+++ LeetCode Solutions/Problems/1276. Number of Burgers with No Waste of Ingredients.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numOfBurgers(self, tomatoSlices: int, cheeseSlices: int) -> List[int]:
+        if tomatoSlices%2 == 1:
+            return []
+        if ((tomatoSlices - 2 * cheeseSlices) / 2) < 0 or ((4 * cheeseSlices - tomatoSlices) / 2) < 0:
+            return []
+        return [(tomatoSlices - 2 * cheeseSlices) // 2, (4 * cheeseSlices - tomatoSlices) // 2]

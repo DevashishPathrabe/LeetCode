f2af17eb666bd8af7d4a84977770750248a28b5e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-06 10:10:35 +0530

    1481. Least Number of Unique Integers after K Removals

---
+++ LeetCode Solutions/Problems/1481. Least Number of Unique Integers after K Removals.py
@@ -0,0 +1,4 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        c = collections.Counter(arr)
+        return len(set(sorted( arr, key=lambda x: (c[x], x), reverse=False)[k:]))

083e899fbc2bc05185d62b0ceb30b6bc56036f49
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-07 10:47:45 +0530

    1884. Egg Drop With 2 Eggs and N Floors

---
+++ LeetCode Solutions/Problems/1884. Egg Drop With 2 Eggs and N Floors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int twoEggDrop(int n) {
+        int minimumNumberOfMoves = 0;
+        int iterable = n;
+        while (iterable > 0){
+            minimumNumberOfMoves += 1;
+            iterable -= minimumNumberOfMoves;
+        }
+        return minimumNumberOfMoves;
+    }
+};

7693bd121d7915a0365239e613649cfbf8164304
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-08 10:46:40 +0530

    2169. Count Operations to Obtain Zero

---
+++ LeetCode Solutions/Problems/2169. Count Operations to Obtain Zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        numberOfOperations = 0
+        while num1 != 0 and num2 != 0:
+            numberOfOperations += 1
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+        return numberOfOperations

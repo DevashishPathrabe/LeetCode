3b595e905bf3b124fb452fe635b231674e2d7feb
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-08 10:52:42 +0530

    2180. Count Integers With Even Digit Sum

---
+++ LeetCode Solutions/Problems/2180. Count Integers With Even Digit Sum.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int countEven(int num) {
+        return (String.valueOf(num).chars().map(Character::getNumericValue).sum() % 2 == 0) ? num / 2: (num - 1) / 2;
+    }
+}

6104a878f426810b4d97814524775f0e01397e04
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-08 10:32:31 +0530

    2144. Minimum Cost of Buying Candies With Discount

---
+++ LeetCode Solutions/Problems/2144. Minimum Cost of Buying Candies With Discount.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int minCost = 0, n = cost.length;
+        for (int i=0; i<n; ++i){
+            if (i % 3 != n % 3){
+                minCost += cost[i];
+            }
+        }
+        return minCost;
+    }
+}

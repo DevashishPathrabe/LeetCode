62ed9fef1d94f186c0104cb4d36a8e619fb45a57
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-08 11:12:23 +0530

    2243. Calculate Digit Sum of a String

---
+++ LeetCode Solutions/Problems/2243. Calculate Digit Sum of a String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def digitSum(self, s: str, k: int) -> str:
+        while len(s) > k:
+            new_s = ''
+            for i in range(0, len(s), k):
+                new_s += str(sum([int(c) for c in s[i:i+k]]))
+            s = new_s[:]
+        return s

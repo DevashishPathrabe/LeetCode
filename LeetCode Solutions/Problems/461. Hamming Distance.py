6ad123aa8d13033223ca5d8dce2dac951ec6851a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-10 10:03:11 +0530

    461. Hamming Distance

---
+++ LeetCode Solutions/Problems/461. Hamming Distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        hammingDistance = 0
+        for i in range(0,32):
+            if((x&1) != (y&1)):
+                hammingDistance += 1
+            x = x>>1
+            y = y>>1
+        return hammingDistance

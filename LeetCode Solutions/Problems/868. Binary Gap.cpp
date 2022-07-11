d07e93664b2fc4db831c184565eb651727d1fb45
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-11 10:35:06 +0530

    868. Binary Gap

---
+++ LeetCode Solutions/Problems/868. Binary Gap.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int longestDistance = 0, flag = 0, distance = 0;
+        while (n != 0) {
+            int t = n % 2;
+            n /= 2;
+            if (t == 1 && flag == 1) {
+                distance++;
+                longestDistance = max(longestDistance, distance);
+                distance = 0;
+            } else if (t == 1 && flag == 0) {
+                flag = 1;
+            } else if (t == 0 && flag == 1) {
+                distance++;
+            }
+        }
+        return longestDistance;
+    }
+};

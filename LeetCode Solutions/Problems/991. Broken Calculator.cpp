81c48eb90b48f9315f2d66a220145a732a47463a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-12 09:05:05 +0530

    991. Broken Calculator

---
+++ LeetCode Solutions/Problems/991. Broken Calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int x, int y) {
+        int result = 0;
+        while(x < y){
+            result++;
+            if(y % 2 > 0){
+                y++;
+            } else{
+                y /= 2;
+            }
+        }
+        return result + x - y;
+    }
+};

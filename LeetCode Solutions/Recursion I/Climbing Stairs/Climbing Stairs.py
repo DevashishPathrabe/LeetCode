338620a43d96f401111e257ed493cfda02acf84c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-12 20:43:03 +0530

    Climbing Stairs

---
+++ LeetCode Solutions/Recursion I/Climbing Stairs/Climbing Stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        num = 0
+        num1 = 1
+        for i in range(n):
+            num2 = num + num1
+            num = num1
+            num1 = num2
+        return (num1)

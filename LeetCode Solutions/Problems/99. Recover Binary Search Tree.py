49d46c8cb6a396de8963e2f507b2306a109ac46f
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-12 09:04:45 +0530

    99. Recover Binary Search Tree

---
+++ LeetCode Solutions/Problems/99. Recover Binary Search Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        first = None
+        second = None
+        previous = TreeNode(-math.inf)
+        def helper(root):
+            nonlocal previous, first, second
+            if root == None:
+                return
+            helper(root.left)
+            if root.val < previous.val:
+                if not first:
+                    first = previous
+                second = root
+            previous = root
+            helper(root.right)
+        helper(root)
+        first.val,second.val = second.val,first.val

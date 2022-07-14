a4fe3cea2767873a020995ee9920a839745a6d15
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-14 09:23:59 +0530

    Best Time to Buy and Sell Stock

---
+++ LeetCode Solutions/Top Interview Questions(Easy Collection)/Dynamic Programming/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if (not prices):
+            return 0
+        buyingPrice = sys.maxsize
+        maximumProfit = 0
+        for i, j in enumerate(prices):
+            if (j < buyingPrice):
+                buyingPrice = j
+            profit = prices[i] - buyingPrice
+            if (profit > maximumProfit):
+                maximumProfit = profit
+        return maximumProfit

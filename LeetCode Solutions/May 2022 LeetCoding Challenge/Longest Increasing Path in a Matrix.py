609e6d310158f2dbecbc88e02a1d6b7baaa3dc3a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-14 09:16:58 +0530

    Longest Increasing Path in a Matrix

---
+++ LeetCode Solutions/May 2022 LeetCoding Challenge/Longest Increasing Path in a Matrix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestIncreasingPath(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        matrix = {i + j*1j: val
+              for i, row in enumerate(matrix)
+              for j, val in enumerate(row)}
+        length = {}
+        for z in sorted(matrix, key=matrix.get):
+            length[z] = 1 + max([length[Z]
+                                 for Z in z+1, z-1, z+1j, z-1j
+                                 if Z in matrix and matrix[z] > matrix[Z]]
+                                or [0])
+        return max(length.values() or [0])

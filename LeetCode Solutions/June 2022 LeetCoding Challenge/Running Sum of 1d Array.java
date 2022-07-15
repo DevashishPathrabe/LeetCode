78e8cfd4e00ec16bd7ca54ca561238e59e55d0e6
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-15 09:51:07 +0530

    Running Sum of 1d Array

---
+++ LeetCode Solutions/June 2022 LeetCoding Challenge/Running Sum of 1d Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] Sum = new int [nums.length];
+        Sum[0] = nums[0];
+        for (int i=1; i<nums.length; i++){
+            Sum[i] = Sum[i-1] + nums[i];
+        }
+        return Sum;
+    }
+}

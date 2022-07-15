e50b88804c7e5b4cf9080ac7e20d1a8a899f97e7
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-15 10:57:28 +0530

    Longest Increasing Subsequence

---
+++ LeetCode Solutions/Top Interview Questions(Medium Collection)/Dynamic Programming/Longest Increasing Subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if(len(nums) == 0):
+            return 0
+        lis = [1]*len(nums)
+        for i in range(1,len(nums)):
+            for j in range(0,i):
+                if(nums[i] > nums[j]):
+                    if(lis[i] <= lis[j]):
+                        lis[i] = lis[j]+1 
+        return max(lis)

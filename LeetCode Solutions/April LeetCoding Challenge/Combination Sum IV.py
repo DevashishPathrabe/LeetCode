e8beaf59bfafebc3d810fd483315773f798dcda5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-16 22:54:50 +0530

    Delete Combination Sum IV.py

--- LeetCode Solutions/April LeetCoding Challenge/Combination Sum IV.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        arr = [0] * (target+1)
-        arr[0] = 1
-        for i in range(1, target+1):
-            for num in nums:
-                if(num <= i):
-                    arr[i] += arr[i-num]
-        return arr[target]

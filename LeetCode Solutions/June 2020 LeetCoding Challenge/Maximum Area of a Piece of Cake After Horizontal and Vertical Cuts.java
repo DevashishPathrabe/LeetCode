eb4c7b9a5d08b4db03f9bf8441e8e933da656f46
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-18 10:02:13 +0530

    Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ LeetCode Solutions/June 2020 LeetCoding Challenge/Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        int maxHorizontalCuts = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length-1]),
+            maxVerticalCuts = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length-1]);
+        for(int i=1; i<horizontalCuts.length; i++){
+            maxHorizontalCuts = Math.max(maxHorizontalCuts, horizontalCuts[i] - horizontalCuts[i-1]);
+        }
+        for(int i=1; i<verticalCuts.length; i++){
+            maxVerticalCuts = Math.max(maxVerticalCuts, verticalCuts[i] - verticalCuts[i-1]);
+        }
+        return (int)((long)maxHorizontalCuts * maxVerticalCuts % 1000000007);
+    }
+}

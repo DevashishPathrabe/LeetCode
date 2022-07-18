e17af623daa34667c91c3163be380297772058fc
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-18 10:04:50 +0530

    Number of Subarrays with Bounded Maximum

---
+++ LeetCode Solutions/June 2020 LeetCoding Challenge/Number of Subarrays with Bounded Maximum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int numberOfSubarrays = 0, lower = 0, middle = 0;
+        for(int num:nums){
+            if(num > right){
+                middle = 0;
+            } else{
+                numberOfSubarrays += ++middle;
+            }
+            if(num >= left){
+                lower = 0;
+            } else{
+                numberOfSubarrays -= ++lower;
+            }
+        }
+        return numberOfSubarrays;
+    }
+};

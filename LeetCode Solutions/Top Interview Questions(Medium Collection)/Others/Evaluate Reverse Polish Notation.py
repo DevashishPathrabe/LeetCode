35831382b1d5ad36b599c48700cac1447f460dee
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-19 11:33:25 +0530

    Evaluate Reverse Polish Notation

---
+++ LeetCode Solutions/Top Interview Questions(Medium Collection)/Others/Evaluate Reverse Polish Notation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        t, f = tokens.pop(), self.evalRPN
+        if(t in '+-*/'):
+            b, a = f(tokens), f(tokens)
+            t = eval('a' + t + 'b')
+        return int(t)

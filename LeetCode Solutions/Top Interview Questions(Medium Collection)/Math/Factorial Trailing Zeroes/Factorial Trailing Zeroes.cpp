fa66be4bd57bdae9e780543a8449cbb30387a890
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-19 11:11:35 +0530

    Factorial Trailing Zeroes

---
+++ LeetCode Solutions/Top Interview Questions(Medium Collection)/Math/Factorial Trailing Zeroes/Factorial Trailing Zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int zeroes = 0;
+        while (n > 4){
+            zeroes += n/5;
+            n /= 5;
+        }
+        return zeroes;
+    }
+};

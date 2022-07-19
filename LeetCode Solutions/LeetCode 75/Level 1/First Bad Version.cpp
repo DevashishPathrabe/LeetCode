c45e1a3f7fcec5abf0c002a0856209361ab40afe
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-19 12:40:37 +0530

    First Bad Version

---
+++ LeetCode Solutions/LeetCode 75/Level 1/First Bad Version.cpp
@@ -0,0 +1,22 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int result;
+        int left = 0;
+        int right = n;
+        while (left <= right){
+            int middle = left + (right - left) / 2;
+            if (isBadVersion(middle)){
+                result = middle;
+                right = middle-1;
+            }
+            else{
+                left = middle+1;
+            }
+        }
+      return result;
+    }
+};

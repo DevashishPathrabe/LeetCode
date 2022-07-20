fc8d086b2d318f4aefc253cb03cbbc685e843060
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-20 11:04:12 +0530

    Number of Steps to Reduce a Number to Zero

---
+++ LeetCode Solutions/February 2020 LeetCoding Challenge/Number of Steps to Reduce a Number to Zero.c
@@ -0,0 +1,14 @@
+
+
+int numberOfSteps (int num){
+    int numberOfSteps = 0;
+    while(num != 0){
+        if(num%2 == 1){
+            num--;
+        } else{
+            num /= 2;
+        }
+        numberOfSteps++;
+    }
+    return numberOfSteps;
+}

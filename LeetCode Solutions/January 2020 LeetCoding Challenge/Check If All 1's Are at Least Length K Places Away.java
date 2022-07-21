135ba237a45d29fb0757bb5e7bdf1d8151a02656
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-21 22:52:11 +0530

    Check If All 1's Are at Least Length K Places Away

---
+++ LeetCode Solutions/January 2020 LeetCoding Challenge/Check If All 1's Are at Least Length K Places Away.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        for(int i=0, count=0; i<nums.length; i++){
+            if(nums[i] == 1 && count < k && i != 0){
+                return false;
+            }
+            else if(nums[i] == 1){
+                count = 0;
+            } else{
+                count++;
+            }
+        }
+        return true;
+    }
+}

df9514dbbb95df94632bf5868d263f70cfb54932
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-22 10:50:21 +0530

    Delete Minimum Moves to Equal Array Elements II.py

--- LeetCode Solutions/May LeetCoding Challenges/Minimum Moves to Equal Array Elements II.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minMoves2(self, nums: List[int]) -> int:
-        nums.sort()
-        minimumNumberOfMoves, median = 0, nums[len(nums) // 2]
-        for num in nums:
-            minimumNumberOfMoves += abs(median - num)
-        return minimumNumberOfMoves

9bceb83cb0aa102dbbe6df501c0082b2cc90f820
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-22 10:10:15 +0530

    Delete Smallest String With A Given Numeric Value.py

--- LeetCode Solutions/January LeetCoding Challenge/Smallest String With A Given Numeric Value.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getSmallestString(self, n: int, k: int) -> str:
-        k -= n
-        numericValue = '_bcdefghijklmnopqrstuvwxy_'
-        lexicographicallySmallestString = 'z' * (k // 25)
-        if(k % 25):
-            lexicographicallySmallestString = numericValue[k%25] + lexicographicallySmallestString
-        return 'a' * (n - len(lexicographicallySmallestString)) + lexicographicallySmallestString

cc32489eff74028b2d7b9173a277bf02e17e96aa
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-25 20:04:35 +0530

    Delete Customer Who Visited but Did Not Make Any Transactions.sql

--- LeetCode Solutions/SQL I/Customer Who Visited but Did Not Make Any Transactions.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-select customer_id, count(customer_id) as count_no_trans from Visits where visit_id not in(select visit_id from Transactions t) group by customer_id;

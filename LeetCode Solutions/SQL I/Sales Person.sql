ae0feb16a2ddc368a3d26ea4190bf9a85741dd60
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-25 20:05:54 +0530

    Delete Sales Person.sql

--- LeetCode Solutions/SQL I/Sales Person.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-select name from salesperson where sales_id not in (select orders.sales_id from orders join company on orders.com_id = company.com_id where company.name = "RED")

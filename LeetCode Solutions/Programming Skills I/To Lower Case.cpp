52be8809e24c9e6f5f3257e302b27436dacd375e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-26 09:53:19 +0530

    Delete To Lower Case.cpp

--- LeetCode Solutions/Programming Skills I/To Lower Case.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    string toLowerCase(string s) {
-        string ans = "";
-        for (auto& c : s)
-            ans += c > 64 && c < 91 ? c + 32 : c;
-        return ans;
-    }
-};

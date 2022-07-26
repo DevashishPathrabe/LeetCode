c91df17f92b60f40187b20086bd743d4c1b8925d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-26 10:02:20 +0530

    Delete Add to Array-Form of Integer.py

--- LeetCode Solutions/Programming Skills II/Add to Array-Form of Integer.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        s = ""
-        for i in num:
-            s += str(i)
-        result = int(s) + k
-        return  list("".join(str(result)))

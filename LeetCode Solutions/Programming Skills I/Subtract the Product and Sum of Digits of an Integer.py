372c93a737d9aaae7980b96183390cd58ed575ee
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-26 09:53:00 +0530

    Delete Subtract the Product and Sum of Digits of an Integer.py

--- LeetCode Solutions/Programming Skills I/Subtract the Product and Sum of Digits of an Integer.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def subtractProductAndSum(self, n: int) -> int:
-        st = str(n) 
-        productNum = 1
-        sumNum = 0
-        for i in st:
-            productNum *= int(i)
-            sumNum += int(i)
-        return productNum - sumNum

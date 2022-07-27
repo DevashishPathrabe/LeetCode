b3bb46f1302a7745155a94be76f81f58018666e0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-27 18:05:47 +0530

    Delete Length of Last Word.py

--- LeetCode Solutions/Programming Skills II/Length of Last Word.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        sList = list(filter(None, s.split(' ')))
-        if(len(sList) == 0):
-            return 0
-        else:
-            return len(sList[-1])

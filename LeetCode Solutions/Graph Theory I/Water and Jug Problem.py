e3b45264eaada36a5c6bdb56bd02284160425cf8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-28 20:50:28 +0530

    Delete Water and Jug Problem.py

--- LeetCode Solutions/Graph Theory I/Water and Jug Problem.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
-        if jug1Capacity == jug2Capacity:
-            return targetCapacity == jug1Capacity
-        if targetCapacity > jug1Capacity + jug2Capacity:
-            return False
-        return targetCapacity % math.gcd(jug1Capacity, jug2Capacity) == 0

260773f56411e852205a9835b005444a0c8299f7
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-07-28 20:49:01 +0530

    Delete Minimum Number of Vertices to Reach All Nodes.py

--- LeetCode Solutions/Graph Theory I/Minimum Number of Vertices to Reach All Nodes.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        result = set(range(n))
-        for x, y in edges:
-            if y in result:
-                result.remove(y)
-        return list(result)

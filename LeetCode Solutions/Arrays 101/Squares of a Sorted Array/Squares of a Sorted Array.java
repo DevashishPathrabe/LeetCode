163f861ae971a349906c0ca9a49c0efb4f223b74
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-01 10:47:55 +0530

    Delete Squares of a Sorted Array.java

--- LeetCode Solutions/Arrays 101/Squares of a Sorted Array/Squares of a Sorted Array.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] sortedSquares(int[] A) {
-        for (int number=0; number<A.length; number++){
-            A[number] = A[number] * A[number];
-        }
-        Arrays.sort(A);
-        return A;
-    }
-}

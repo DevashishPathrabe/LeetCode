7070a30422c1f607abe81bca62e1b6d38c6ccf30
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-01 10:45:49 +0530

    Delete Height Checker.py

--- LeetCode Solutions/Arrays 101/Height Checker/Height Checker.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        array = sorted(heights)
-        numberOfStudents = 0
-        for i in range(len(array)):
-            if (array[i] != heights[i]):
-                numberOfStudents += 1
-        return numberOfStudents

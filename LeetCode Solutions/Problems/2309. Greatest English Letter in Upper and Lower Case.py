affac38db786046866cd8ed13cbee65defa039e8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-02 11:14:09 +0530

    2309. Greatest English Letter in Upper and Lower Case

---
+++ LeetCode Solutions/Problems/2309. Greatest English Letter in Upper and Lower Case.py
@@ -0,0 +1,7 @@
+class Solution:
+    def greatestLetter(self, s: str) -> str:
+        s = sorted(s)
+        for i in reversed(range(len(s))): 
+            if s[i].upper() in s and s[i].lower() in s:
+                return s[i].upper()
+        return ""

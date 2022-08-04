a6aba89a1f5f73cce84fd1687d7e50462f9a6009
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-04 10:12:14 +0530

    Delete Pascal's Triangle II.py

--- LeetCode Solutions/Recursion I/Pascal's Triangle II/Pascal's Triangle II.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        s = 1
-        res = [s]
-        for i in range(rowIndex):
-            s = s * (rowIndex-i) // (i+1)
-            res.append(s)
-        return res

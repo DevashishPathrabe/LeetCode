7ff4dcc59d1556e9b5b477ec2802e87a5fbdc25c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-07 10:42:11 +0530

    Sqrt(x)

---
+++ LeetCode Solutions/Learn/Binary Search/Sqrt(x)/Sqrt(x).c
@@ -0,0 +1,12 @@
+
+
+int mySqrt(int x){
+    if (x <= 1){
+        return x;
+    }
+    long sqrt = x; 
+    while (sqrt*sqrt > x){
+        sqrt = (sqrt + x/sqrt) / 2;
+    }
+    return (int) sqrt;
+}

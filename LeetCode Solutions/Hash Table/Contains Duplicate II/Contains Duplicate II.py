06b6d7cfac4776789cea4edd69735ced72fdaa60
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-08 08:44:36 +0530

    Delete Contains Duplicate II.py

--- LeetCode Solutions/Hash Table/Contains Duplicate II/Contains Duplicate II.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        dict = {}
-        for i in range(len(nums)):
-            if (nums[i] in dict and abs(i-dict[nums[i]]) <= k):
-                return True
-            dict[nums[i]] = i
-        return False

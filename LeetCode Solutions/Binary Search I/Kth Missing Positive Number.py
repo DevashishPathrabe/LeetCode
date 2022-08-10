f677377bdbc826dc59a48095755d551986d85296
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-10 19:19:36 +0530

    Delete Kth Missing Positive Number.py

--- LeetCode Solutions/Binary Search I/Kth Missing Positive Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findKthPositive(self, arr: List[int], k: int) -> int:
-        count = 0
-        for i in range(1, arr[-1]+1):
-            if(i not in arr):
-                count += 1
-                if(count == k):
-                    return i
-        return arr[-1]+(k-count)

6d9261c74577dd5f9f293d09e063a0832fefacdf
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-10 19:32:57 +0530

    Delete H-Index II.py

--- LeetCode Solutions/Binary Search II/H-Index II.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        for i in range(len(citations)):
-            if citations[i] >= len(citations) - i:
-                return len(citations) - i
-        return 0

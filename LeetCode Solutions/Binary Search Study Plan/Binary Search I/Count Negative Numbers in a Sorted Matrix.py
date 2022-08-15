409acd914f019e4cec8a9cd230b64ead5d5ff98e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-08-15 18:42:41 +0530

    Delete Count Negative Numbers in a Sorted Matrix.py

--- LeetCode Solutions/Binary Search Study Plan/Binary Search I/Count Negative Numbers in a Sorted Matrix.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
-        result = sum(grid, [])
-        count = 0
-        for i in result:
-            if i < 0:
-                count +=1
-        return count

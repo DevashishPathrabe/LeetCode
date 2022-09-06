87c31ab0c1abaafabd1efb75cab3ba5fc310486d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-06 10:37:52 +0530

    Create Remove Linked List Elements.c

---
+++ LeetCode Solutions/Linked List/Remove Linked List Elements.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode *curr = head;
+    struct ListNode *prev = NULL;
+    while(curr != NULL){
+        if(curr->val == val){
+            if(curr == head){
+                head = head->next;
+                curr = head;
+            } else{
+                prev->next = curr->next;
+                curr = prev->next;
+            }
+        }
+        else{
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+    return head;
+}

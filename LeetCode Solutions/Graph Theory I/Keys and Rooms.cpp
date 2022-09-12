7157a7505049b04754c9d0ef53aa3529489791d5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-12 10:10:21 +0530

    Create Keys and Rooms.cpp

---
+++ LeetCode Solutions/Graph Theory I/Keys and Rooms.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> arr(rooms.size(), false);
+        arr[0] = true;
+        stack<int> s = stack<int>({0});
+        int count = 1;
+        while(s.size()){
+            vector<int> keys = rooms[s.top()];
+            s.pop();
+            for(int k : keys){
+                if(!arr[k]){
+                    s.push(k), arr[k] = true, count++;
+                }
+            }
+        }
+        return rooms.size() == count;
+    }
+};

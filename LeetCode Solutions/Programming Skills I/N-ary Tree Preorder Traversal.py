bc1ebb1fcdcbc282fd6e3c2c56a902365941a3ba
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-14 10:31:05 +0530

    Create N-ary Tree Preorder Traversal.py

---
+++ LeetCode Solutions/Programming Skills I/N-ary Tree Preorder Traversal.py
@@ -0,0 +1,17 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if(not root):
+            return []
+        resultTree = [root.val]
+        if(root.children):
+            for child in root.children:
+                resultTree += self.preorder(child)
+        return resultTree

797611d4fb2e91dff55cd8acc349fd7be0737177
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-16 11:41:22 +0530

    Create Sales Person.sql

---
+++ LeetCode Solutions/SQL I/Sales Person.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select name from salesperson where sales_id not in (select orders.sales_id from orders join company on orders.com_id = company.com_id where company.name = "RED")

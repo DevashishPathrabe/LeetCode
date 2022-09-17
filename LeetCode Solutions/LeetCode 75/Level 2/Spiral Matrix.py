03c177b55b06f5cda5c3a542b0655a668c9ca70b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-17 11:21:30 +0530

    Delete Spiral Matrix.py

--- LeetCode Solutions/LeetCode 75/Level 2/Spiral Matrix.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        out, arr = [], np.array(matrix)
-        while arr.size:
-            out.append(arr[0])
-            arr = arr[1:].T[::-1]
-        return np.concatenate(out)

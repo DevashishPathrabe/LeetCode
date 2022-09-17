acfbdc7be1104550d03f0aff45856ba39c7443fb
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-17 11:20:12 +0530

    Delete Longest Common Prefix.py

--- LeetCode Solutions/LeetCode 75/Level 2/Longest Common Prefix.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 0:
-            return ""
-        i = 0
-        s1, s2 = min(strs), max(strs)
-        while (i < len(s1) and i < len(s2) and s1[i] == s2[i]):
-            i += 1
-        return s1[:i]

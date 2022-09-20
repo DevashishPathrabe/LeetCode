1beb14b76434d912a0df68ec779e9bc702906d52
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-09-20 16:12:48 +0530

    Delete Merge Intervals.py

--- LeetCode Solutions/LeetCode 75/Level 2/Merge Intervals.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        array = []
-        for i in sorted(intervals):
-            if (len(array) == 0 or array[-1][1] < i[0]):
-                array.append(i)
-            else:
-                array[-1][1] = max(array[-1][1], i[1])
-        return array

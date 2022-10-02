27ffb3d07f4788962919c66200a6fa18ffb979f8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-02 10:19:08 +0530

    Word Subsets

---
+++ LeetCode Solutions/March 2021 LeetCoding Challenge/Word Subsets/Word Subsets.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
+        Bfrequency = Counter()
+        for word in B:
+            Bfrequency |= Counter(word)
+        if (sum(Bfrequency.values()) > 10):
+            return []
+        return [word for word in A if(not Bfrequency - Counter(word))]

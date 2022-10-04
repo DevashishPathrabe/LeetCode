6d8841670874fd068e69b282ba51160b9b53adee
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-04 10:49:28 +0530

    Delete Count All Valid Pickup and Delivery Options.py

--- LeetCode Solutions/March 2022 LeetCoding Challenges/Count All Valid Pickup and Delivery Options/Count All Valid Pickup and Delivery Options.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countOrders(self, n: int) -> int:
-        result = 1
-        p = 10**9+7
-        for i in range(1, n+1):
-            result *= i * (2*i - 1)
-            result = result % p
-        return result

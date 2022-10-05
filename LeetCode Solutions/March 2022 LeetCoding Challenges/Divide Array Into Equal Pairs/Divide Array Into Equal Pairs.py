545a1c079f6653e1c68851320220c3c09d940744
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-05 08:47:01 +0530

    Delete Divide Array Into Equal Pairs.py

--- LeetCode Solutions/March 2022 LeetCoding Challenges/Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def divideArray(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                seen.discard(num)
-            else:
-                seen.add(num)
-        return not seen

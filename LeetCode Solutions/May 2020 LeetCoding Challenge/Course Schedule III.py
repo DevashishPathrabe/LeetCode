deeb7d59cb963f5630db8ee11d65b394bea12dff
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-06 08:57:23 +0530

    Course Schedule III

---
+++ LeetCode Solutions/May 2020 LeetCoding Challenge/Course Schedule III.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        heap, total = [], 0
+        for duration, end in sorted(courses, key=lambda el: el[1]):
+            if(duration + total <= end):
+                total += duration
+                heappush(heap, -duration)
+            elif(heap and -heap[0] > duration):
+                total += duration + heappop(heap)
+                heappush(heap, -duration)
+        return len(heap)

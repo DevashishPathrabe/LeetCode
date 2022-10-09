4fbfed81627e0f928c997c2f03b463366e085198
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-09 09:49:40 +0530

    Delete Maximum Gap.py

--- LeetCode Solutions/May 2021 LeetCoding Challenges/Maximum Gap/Maximum Gap.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maximumGap(self, nums: List[int]) -> int:
-        if(len(nums) <= 1):
-            return 0
-        nums.sort()
-        maximumDifference = 0
-        for num in range(1, len(nums)):
-            maximumDifference = max(maximumDifference, nums[num]-nums[num - 1])
-        return maximumDifference

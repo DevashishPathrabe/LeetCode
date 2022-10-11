f84075a8df4b75b14c76648f2a01122e429c5ee2
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-11 10:10:58 +0530

    Delete Climbing Stairs.py

--- LeetCode Solutions/October 2021 LeetCoding Challenges/Climbing Stairs/Climbing Stairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        num1 = 0
-        num2 = 1
-        for i in range(n):
-            num3 = num1 + num2
-            num1 = num2
-            num2 = num3
-        return num2

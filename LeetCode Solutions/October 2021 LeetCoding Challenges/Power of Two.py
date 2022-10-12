6d1b29d63430d626495ec269bb104ca5dd44e0f1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-12 10:48:02 +0530

    Delete Power of Two.py

--- LeetCode Solutions/October 2021 LeetCoding Challenges/Power of Two.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        List = []
-        for i in range(0, 100):
-            x = 2**i
-            List.append(x)
-        if n in List:
-            return True
-        return False

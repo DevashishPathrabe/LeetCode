8b022a1f46e7099ac26c7111d413ec58ce7e0cca
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-14 10:32:04 +0530

    Delete Push Dominoes.py

--- LeetCode Solutions/September 2022 LeetCoding Challenges/Push Dominoes.py
+++
@@ -1,7 +0,0 @@
-temp = ''
-        while dominoes != temp:
-            temp = dominoes
-            dominoes = dominoes.replace('R.L', 'xxx')
-            dominoes = dominoes.replace('R.', 'RR')
-            dominoes = dominoes.replace('.L', 'LL')
-        return  dominoes.replace('xxx', 'R.L')

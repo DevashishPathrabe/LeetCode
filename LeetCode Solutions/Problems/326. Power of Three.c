8e4b1d11cbbe4c1dcf335f38e1f28c445e080f62
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-19 09:12:29 +0530

    326. Power of Three

--- LeetCode Solutions/Problems/326. Power of Three.c
+++ LeetCode Solutions/Problems/326. Power of Three.c
@@ -1,14 +1,6 @@
-
-
 bool isPowerOfThree(int n){
-    if(n == 0){
+    if (n == 0){
         return false;
     }
-    while(n != 1){
-        if(n%3 != 0){
-            return false;
-        }
-        n /= 3;
-    }
-    return true;
+    return (n == 1) || (n % 3 == 0 && isPowerOfThree(n / 3));
 }

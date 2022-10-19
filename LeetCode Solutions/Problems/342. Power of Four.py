8e14cead66fee200525d3e53fc67f29efe20f8a3
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-19 09:12:59 +0530

    342. Power of Four

--- LeetCode Solutions/Problems/342. Power of Four.py
+++ LeetCode Solutions/Problems/342. Power of Four.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        while n >= 1:
-            if n == 1:
-                return True
-            n /= 4
-        return False
+        if (n == 0):
+            return False
+        return (n == 1) or (n % 4 == 0 and self.isPowerOfFour(n // 4))

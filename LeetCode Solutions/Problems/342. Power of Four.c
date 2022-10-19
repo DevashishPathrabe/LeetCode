6557f3d668c6740661b529ca77ba862c0cbcaa8d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-19 09:12:43 +0530

    342. Power of Four

--- LeetCode Solutions/Problems/342. Power of Four.c
+++ LeetCode Solutions/Problems/342. Power of Four.c
@@ -1,15 +1,6 @@
-
-
 bool isPowerOfFour(int n){
-    if (n <= 0) {
+    if (n == 0){
         return false;
     }
-    while (n > 1) {
-        if (n % 4 == 0) {
-            n /= 4;
-        } else {
-            return false;
-        }
-    }
-    return true;
+    return (n == 1) || ((n % 4 == 0) && isPowerOfFour(n / 4));
 }

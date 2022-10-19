e02b0b3aa75374d041a5bf9fb07f77e5b3dd88dc
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-19 09:12:53 +0530

    342. Power of Four

--- LeetCode Solutions/Problems/342. Power of Four.java
+++ LeetCode Solutions/Problems/342. Power of Four.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n <= 0) {
+        if (n == 0){
             return false;
         }
-        while (n > 1) {
-            if (n % 4 == 0) {
-                n /= 4;
-            } else {
-                return false;
-            }
-        }
-        return true;
+        return (n == 1) || ((n % 4 == 0) && isPowerOfFour(n / 4));
     }
 }

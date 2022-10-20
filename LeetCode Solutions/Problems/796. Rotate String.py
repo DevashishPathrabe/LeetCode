aa4644a3a4ceee38028ae873abeada5f544e42c3
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-20 16:49:06 +0530

    796. Rotate String

--- LeetCode Solutions/Problems/796. Rotate String.py
+++ LeetCode Solutions/Problems/796. Rotate String.py
@@ -1,6 +1,3 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        for i in range(len(s) + 1):
-            if s[i:] + s[:i] == goal:
-                return True
-        return False
+        return (s in (goal * 2) and len(s) == len(goal))

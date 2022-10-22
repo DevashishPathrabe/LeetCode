5bc2d4de423aed716e2d822c03a1eaf858bc6725
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-22 09:57:33 +0530

    69. Sqrt(x)

--- LeetCode Solutions/Problems/69. Sqrt(x).java
+++ LeetCode Solutions/Problems/69. Sqrt(x).java
@@ -1,12 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x <= 1){
-            return x;
-        }
-        long sqrt = x; 
-        while(sqrt*sqrt > x){
-        	sqrt = (sqrt + x/sqrt) / 2;
-        }    	
-        return (int) sqrt;
+        return (int)(Math.sqrt(x));
     }
 }

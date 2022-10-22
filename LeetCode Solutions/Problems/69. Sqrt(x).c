80cbd575b9ef12f6b6ee436343412cf202b07440
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-22 09:57:27 +0530

    69. Sqrt(x)

--- LeetCode Solutions/Problems/69. Sqrt(x).c
+++ LeetCode Solutions/Problems/69. Sqrt(x).c
@@ -1,12 +1,5 @@
 
 
 int mySqrt(int x){
-    if(x <= 1){
-        return x;
-    }
-    long sqrt = x; 
-    while(sqrt*sqrt > x){
-        sqrt = (sqrt + x/sqrt) / 2;
-    }
-    return (int) sqrt;
+    return (int)(sqrt(x));
 }

274147fa660b708a5983d9f0b9476e4048d1fd56
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-22 09:57:11 +0530

    389. Find the Difference

--- LeetCode Solutions/Problems/389. Find the Difference.py
+++ LeetCode Solutions/Problems/389. Find the Difference.py
@@ -1,8 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sum1, sum2 = 0, 0
-        for i in s:
-            sum1 = sum1 + ord(i)
         for i in t:
-            sum2 = sum2 + ord(i)
-        return (chr(sum2 - sum1))
+            if s.count(i) != t.count(i):
+                return i

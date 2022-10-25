f7d8272e6024dbabb70643ec52224572510ae9c8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-25 09:57:34 +0530

    414. Third Maximum Number

--- LeetCode Solutions/Problems/414. Third Maximum Number.cpp
+++ LeetCode Solutions/Problems/414. Third Maximum Number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         int i=1;
-        for(int j=1; j<nums.size(); j++) 
-            if(nums[j] != nums[j-1]) 
+        for (int j=1; j<nums.size(); j++) 
+            if (nums[j] != nums[j-1]) 
                 nums[i++] = nums[j];
-        if(i < 3){
+        if (i < 3){
             return nums[i-1];
         }
         return nums[i-3];

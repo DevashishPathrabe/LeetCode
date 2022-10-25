7b2767a4938413192f445c214c10175a34de2c1f
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-25 09:57:21 +0530

    242. Valid Anagram

--- LeetCode Solutions/Problems/242. Valid Anagram.java
+++ LeetCode Solutions/Problems/242. Valid Anagram.java
@@ -1,17 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] alphabet = new int[26];
-        for(int i=0; i<s.length(); i++){
-            alphabet[s.charAt(i) - 'a']++;
-        }
-        for(int i=0; i<t.length(); i++){
-            alphabet[t.charAt(i) - 'a']--;
-        }
-        for(int i : alphabet){
-            if(i != 0){
-                return false;
-            }
-        }
-        return true;
+        char sArray[] = s.toCharArray();
+        Arrays.sort(sArray);
+        char tArray[] = t.toCharArray();
+        Arrays.sort(tArray);
+        return (Arrays.equals(sArray, tArray));
     }
 }

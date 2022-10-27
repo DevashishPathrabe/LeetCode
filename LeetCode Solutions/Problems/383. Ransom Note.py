6a05ed6909eb412cb05c7495ad6b154e0c5482e9
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-27 09:44:56 +0530

    383. Ransom Note

--- LeetCode Solutions/Problems/383. Ransom Note.py
+++ LeetCode Solutions/Problems/383. Ransom Note.py
@@ -1,14 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        table = {}
-        for i in magazine:
-            if i in table:
-                table[i] += 1
-            else:
-                table[i] = 1
-        for i in ransomNote:
-            if i in table and table[i] > 0:
-                table[i] -= 1
-            else:
-                return False
-        return True
+        return all(ransomNote.count(i) <= magazine.count(i) for i in ransomNote)

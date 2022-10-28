8f9473813dad91743f0a9102ae1835bf40560a71
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-28 10:36:14 +0530

    509. Fibonacci Number

--- LeetCode Solutions/Problems/509. Fibonacci Number.cpp
+++ LeetCode Solutions/Problems/509. Fibonacci Number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    int fib(int N) {
-        if(N < 2){
-            return N;
-        } else{
-            return fib(N-1) + fib(N-2);
+    int fib(int n) {
+        if (n == 0){
+            return 0;
         }
+        if (n == 1){
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 };

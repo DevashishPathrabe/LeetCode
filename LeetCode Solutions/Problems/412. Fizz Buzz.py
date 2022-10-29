b50c74c2d8a99f0cf2326f46ec1394d3406eb02e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-29 16:02:32 +0530

    412. Fizz Buzz

--- LeetCode Solutions/Problems/412. Fizz Buzz.py
+++ LeetCode Solutions/Problems/412. Fizz Buzz.py
@@ -1,13 +1,13 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        resultString = []
+        answer = []
         for i in range(1, n+1):
-            if(i%3 == 0 and i%5 == 0):
-                resultString.append('FizzBuzz')
-            elif(i%5 == 0):
-                resultString.append('Buzz')
-            elif(i%3 == 0):
-                resultString.append('Fizz')
+            if i%3 == 0 and i%5 == 0:
+                answer.append("FizzBuzz")
+            elif i%3 == 0:
+                answer.append("Fizz")
+            elif i%5 == 0:
+                answer.append("Buzz")
             else:
-                resultString.append(str(i))
-        return resultString
+                answer.append(str(i))
+        return answer

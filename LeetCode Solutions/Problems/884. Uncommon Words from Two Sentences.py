39b71c763ff45c7dead4779c640bc52786f39fc5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-01 09:44:40 +0530

    884. Uncommon Words from Two Sentences

--- LeetCode Solutions/Problems/884. Uncommon Words from Two Sentences.py
+++ LeetCode Solutions/Problems/884. Uncommon Words from Two Sentences.py
@@ -1,4 +1,4 @@
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        lst = s1.split()+s2.split()
-        return [i for i in lst if lst.count(i)==1]
+        List = s1.split() + s2.split()
+        return [i for i in List if List.count(i) == 1]

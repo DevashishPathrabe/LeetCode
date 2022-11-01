8ce8f59d637a4c21f0397b440bbe1f58af11bfea
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-01 09:43:45 +0530

    821. Shortest Distance to a Character

--- LeetCode Solutions/Problems/821. Shortest Distance to a Character.py
+++ LeetCode Solutions/Problems/821. Shortest Distance to a Character.py
@@ -2,12 +2,12 @@ class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         answer, distance = [10001]*len(s), 10001
         for i in range(len(s)):
-            if(s[i] == c):
+            if s[i] == c:
                 distance = 0
             answer[i] = distance
             distance += 1
         for i in range(len(s)-1, -1, -1):
-            if(s[i] == c):
+            if s[i] == c:
                 distance = 0
             answer[i] = min(answer[i], distance)
             distance += 1

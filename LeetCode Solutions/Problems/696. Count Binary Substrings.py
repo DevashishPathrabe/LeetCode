b4b97c087422031d573ce8b1e4c23eff852b2200
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-01 09:39:54 +0530

    696. Count Binary Substrings

--- LeetCode Solutions/Problems/696. Count Binary Substrings.py
+++ LeetCode Solutions/Problems/696. Count Binary Substrings.py
@@ -2,7 +2,7 @@ class Solution:
     def countBinarySubstrings(self, s: str) -> int:
         current, previous, result = 1, 0, 0
         for i in range(1, len(s)):
-            if(s[i] == s[i-1]):
+            if s[i] == s[i-1]:
                 current += 1
             else:
                 result += min(current, previous)

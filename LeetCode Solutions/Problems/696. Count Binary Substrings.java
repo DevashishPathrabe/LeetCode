169fb84b12834ce025727ce381ea92e810b66841
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-01 09:39:44 +0530

    696. Count Binary Substrings

--- LeetCode Solutions/Problems/696. Count Binary Substrings.java
+++ LeetCode Solutions/Problems/696. Count Binary Substrings.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countBinarySubstrings(String s) {
         int current = 1, previous = 0, result = 0;
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i) == s.charAt(i-1)){
+        for (int i=1; i<s.length(); i++){
+            if (s.charAt(i) == s.charAt(i-1)){
                 current++;
             } else{
                 result += Math.min(current, previous);

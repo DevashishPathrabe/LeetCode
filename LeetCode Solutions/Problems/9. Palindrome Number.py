35659afec6c6f7b7e993e3da8b42878016f64f69
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-02 11:51:22 +0530

    9. Palindrome Number

--- LeetCode Solutions/Problems/9. Palindrome Number.py
+++ LeetCode Solutions/Problems/9. Palindrome Number.py
@@ -1,12 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        temp = x
-        reverseNumber = 0
-        while x > 0:
-            remainder = x % 10
-            reverseNumber = (reverseNumber * 10) + remainder
-            x = x // 10
-        if temp == reverseNumber:
-            return True
-        else:
-            return False
+        x = str(x)
+        return x == x[::-1]

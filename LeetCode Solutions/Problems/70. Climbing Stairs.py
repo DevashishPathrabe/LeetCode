efd4925887a7dfd00d94f99901dfef6720488463
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-02 11:50:58 +0530

    70. Climbing Stairs

--- LeetCode Solutions/Problems/70. Climbing Stairs.py
+++ LeetCode Solutions/Problems/70. Climbing Stairs.py
@@ -1,9 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        num = 0
-        num1 = 1
+        num1, num2 = 0, 1
         for i in range(n):
-            num2 = num + num1
-            num = num1
+            num3 = num1 + num2
             num1 = num2
-        return(num1)
+            num2 = num3
+        return num3

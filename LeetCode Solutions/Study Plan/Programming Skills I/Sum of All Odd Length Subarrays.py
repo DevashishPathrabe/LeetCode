54f74d0867e0f2ea28c5aa9d9d6d2a0f1c7120c6
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-02 11:45:43 +0530

    Delete Sum of All Odd Length Subarrays.py

--- LeetCode Solutions/Study Plan/Programming Skills I/Sum of All Odd Length Subarrays.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        subarraysSum = 0
-        for i in range(len(arr) + 1):
-            for j in range(i):
-                x = arr[j:i]
-                if x and len(x) % 2 != 0:
-                    subarraysSum += sum(x)
-        return subarraysSum

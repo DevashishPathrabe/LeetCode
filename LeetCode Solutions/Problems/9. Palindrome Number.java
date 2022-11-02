2c3ba77eee521abb50fff3d823c47ead3d395329
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-02 11:51:16 +0530

    9. Palindrome Number

---
+++ LeetCode Solutions/Problems/9. Palindrome Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        long reverseNumber = 0;
+        int temp = x;
+        while (x > 0){
+            int remainder = x % 10;
+            reverseNumber = reverseNumber * 10 + remainder;
+            x /= 10;
+        }
+        if (temp == reverseNumber){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

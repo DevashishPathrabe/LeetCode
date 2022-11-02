34deed2a33b35689db26d886bc1567d7adae1d29
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-02 11:50:53 +0530

    70. Climbing Stairs

--- LeetCode Solutions/Problems/70. Climbing Stairs.java
+++ LeetCode Solutions/Problems/70. Climbing Stairs.java
@@ -2,11 +2,11 @@ class Solution {
     public int climbStairs(int n) {
         int num = 0;
         int num1 = 1;
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             int num2 = num + num1;
             num = num1;
             num1 = num2;
         }
-        return(num1);
+        return num1;
     }
 }

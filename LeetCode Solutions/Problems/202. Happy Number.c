703c588ce8a8c60b00b784896fc91224ec61acef
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-03 08:09:44 +0530

    202. Happy Number

--- LeetCode Solutions/Problems/202. Happy Number.c
+++ LeetCode Solutions/Problems/202. Happy Number.c
@@ -2,9 +2,9 @@
 
 bool isHappy(int n){
     int i=0;
-    while(n!=1 && i<50){
+    while (n!=1 && i<50){
         int x = 0;
-        while(n != 0){
+        while (n != 0){
             x += pow((n%10), 2);
             n /= 10;
             i++;

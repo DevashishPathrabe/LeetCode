b744461ebcc69b95821509a8d575ec355eba1275
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-08 09:48:10 +0530

    Delete Brick Wall.py

--- LeetCode Solutions/April 2021 LeetCoding Challenge/Brick Wall/Brick Wall.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def leastBricks(self, wall: List[List[int]]) -> int:
-        frequency = defaultdict(int)
-        for row in wall:
-            rowSum = row[0]
-            for i in range(1, len(row)):
-                frequency[rowSum] += 1
-                rowSum += row[i]
-        return len(wall) - max(frequency.values() or [0])

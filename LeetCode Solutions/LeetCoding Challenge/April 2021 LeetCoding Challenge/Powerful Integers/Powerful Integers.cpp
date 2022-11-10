bbbd082c239006205b78aa47e5eab768c2541360
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-10 10:08:56 +0530

    Powerful Integers

---
+++ LeetCode Solutions/LeetCoding Challenge/April 2021 LeetCoding Challenge/Powerful Integers/Powerful Integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        unordered_set<int> listOfPowerfullIntegers;
+        for (int xi=1; xi<bound; xi*=x){
+            for (int yj=1; xi+yj<=bound; yj*=y){
+                listOfPowerfullIntegers.insert(xi+yj);
+                if (y == 1){
+                    break;
+                }
+            }
+            if (x == 1){
+                break;
+            }
+        }
+        return vector<int>(listOfPowerfullIntegers.begin(), listOfPowerfullIntegers.end());
+    }
+};

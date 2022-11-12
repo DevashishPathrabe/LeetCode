2ef96c1f031b58c4210a22218736d96ad7ec10e4
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-12 11:52:58 +0530

    30. Substring with Concatenation of All Words

---
+++ LeetCode Solutions/Problems/30. Substring with Concatenation of All Words.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        l, s_len, word_len, words_len = 0, len(s), len(words[0]), len(words)
+        concat_len = word_len*words_len
+        words_counter, result = Counter(words), []
+        for r in range(concat_len-1, s_len):
+            seen = Counter()
+            for k in range(l, r+1, word_len):
+                word = s[k:k+word_len]
+                seen[word] += 1
+                if words_counter[word] < seen[word]:
+                    break
+            else:
+                result.append(l)
+            l += 1
+        return result

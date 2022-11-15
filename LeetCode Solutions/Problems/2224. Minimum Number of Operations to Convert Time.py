56de66b2f20b2ec377b13609c943dd9e62a1bb65
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-15 13:20:50 +0530

    2224. Minimum Number of Operations to Convert Time

--- LeetCode Solutions/Problems/2224. Minimum Number of Operations to Convert Time.py
+++ LeetCode Solutions/Problems/2224. Minimum Number of Operations to Convert Time.py
@@ -1,10 +1,10 @@
 class Solution:
     def convertTime(self, current: str, correct: str) -> int:
-        current_time = 60 * int(current[0:2]) + int(current[3:5])
-        target_time = 60 * int(correct[0:2]) + int(correct[3:5])
-        diff = target_time - current_time
+        currentTime = 60 * int(current[0:2]) + int(current[3:5])
+        targetTime = 60 * int(correct[0:2]) + int(correct[3:5])
+        difference = targetTime - currentTime
         count = 0
         for i in [60, 15, 5, 1]:
-            count += diff // i
-            diff %= i
+            count += difference // i
+            difference %= i
         return count

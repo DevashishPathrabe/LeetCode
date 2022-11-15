e74f01fc6e4bee223be70560c55e35cd9a898bc2
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-15 13:21:22 +0530

    2255. Count Prefixes of a Given String

--- LeetCode Solutions/Problems/2255. Count Prefixes of a Given String.cpp
+++ LeetCode Solutions/Problems/2255. Count Prefixes of a Given String.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int countPrefixes(vector<string>& words, string s) {
         int result = 0;
-        for (auto& w : words)
+        for (auto& w : words){
             result += s.find(w) < 1;
+        }
         return result;
     }
 };

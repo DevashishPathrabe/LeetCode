1a65b27f62ed3dcf08f540eea3e1670ce7b58d41
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-15 13:21:33 +0530

    2255. Count Prefixes of a Given String

--- LeetCode Solutions/Problems/2255. Count Prefixes of a Given String.java
+++ LeetCode Solutions/Problems/2255. Count Prefixes of a Given String.java
@@ -1,9 +1,11 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
         int result = 0;
-        for (String w : words)
-            if (s.startsWith(w))
+        for (String w : words){
+            if (s.startsWith(w)){
                 result++;
+            }
+        }
         return result;
     }
 }

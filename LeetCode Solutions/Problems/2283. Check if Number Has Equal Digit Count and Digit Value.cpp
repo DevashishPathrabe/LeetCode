5cbe1ff027f29b81a1ca3d57c306ea6b37b3fb5e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-15 13:23:03 +0530

    2283. Check if Number Has Equal Digit Count and Digit Value

---
+++ LeetCode Solutions/Problems/2283. Check if Number Has Equal Digit Count and Digit Value.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        const size_t totalDigits = 10;
+        array<int, totalDigits> frequency{};
+        for (int i=0; i<num.length(); ++i){
+            frequency[num[i] - '0']++;
+        }
+        for (int i=0; i<num.length(); ++i){
+            if (frequency[i] != (num[i] - '0')){
+                return false;
+            }
+        }
+        return true;
+    }
+};

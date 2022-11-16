a8f5c99a2d390eba72b695f5a1192d6f997bfc54
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-16 14:54:43 +0530

    374. Guess Number Higher or Lower

--- LeetCode Solutions/Problems/374. Guess Number Higher or Lower.cpp
+++ LeetCode Solutions/Problems/374. Guess Number Higher or Lower.cpp
@@ -1,8 +1,8 @@
 /** 
  * Forward declaration of guess API.
  * @param  num   your guess
- * @return 	     -1 if num is lower than the guess number
- *			      1 if num is higher than the guess number
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
  *               otherwise return 0
  * int guess(int num);
  */
@@ -11,9 +11,9 @@ class Solution {
 public:
     int guessNumber(int n) {
         int left = 1, right = n;
-        while(left < right){
+        while (left < right){
             int mid = left + (right - left) / 2;
-            if(guess(mid) == 0 || guess(mid) == -1){
+            if (guess(mid) == 0 || guess(mid) == -1){
                 right = mid;
             }
             else{

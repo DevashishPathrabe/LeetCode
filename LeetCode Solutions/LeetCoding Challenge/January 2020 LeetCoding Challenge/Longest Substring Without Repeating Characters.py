34aad1651e27671af887686719294655d8960467
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-17 11:45:40 +0530

    Longest Substring Without Repeating Characters

---
+++ LeetCode Solutions/LeetCoding Challenge/January 2020 LeetCoding Challenge/Longest Substring Without Repeating Characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i, j, length = 0, 0, 0
+        Hashset = set()
+        while(i<len(s) and j<len(s)):
+            if(s[j] not in Hashset):
+                Hashset.add(s[j])
+                length = max(length, j-i+1)
+                j += 1
+            else:
+                Hashset.remove(s[i])
+                i += 1
+        return length

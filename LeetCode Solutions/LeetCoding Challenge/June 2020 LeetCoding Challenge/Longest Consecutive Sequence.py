022618cf1558306cfe74177799c6ad478522c649
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-19 15:33:03 +0530

    Longest Consecutive Sequence

---
+++ LeetCode Solutions/LeetCoding Challenge/June 2020 LeetCoding Challenge/Longest Consecutive Sequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        longestConsecutive, c = 0, 0
+        for i in nums:
+            if(i-1 not in s):
+                k = i
+                while(k in s):
+                    c += 1
+                    k += 1
+                longestConsecutive = max(c, longestConsecutive)
+                c = 0
+        return longestConsecutive

f057f5914625e08938d8ccc7790bf010d0c3500b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-12-03 20:05:02 +0530

    Best Time to Buy and Sell Stock with Transaction Fee

---
+++ LeetCode Solutions/LeetCoding Challenge/March 2021 LeetCoding Challenge/Best Time to Buy and Sell Stock with Transaction Fee/Best Time to Buy and Sell Stock with Transaction Fee.c
@@ -0,0 +1,10 @@
+
+
+int maxProfit(int* prices, int pricesSize, int fee){
+    int buying = 0, selling = -prices[0];
+    for (int i=1; i<pricesSize; i++){
+        buying = fmax(buying, selling + prices[i] - fee);
+        selling = fmax(selling, buying - prices[i]);
+    }
+    return buying;
+}

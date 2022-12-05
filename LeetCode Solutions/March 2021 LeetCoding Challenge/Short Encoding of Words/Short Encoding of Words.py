f333c6773072d42ac63e63afcb4a2499c12ac2de
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-12-05 09:58:25 +0530

    Delete Short Encoding of Words.py

--- LeetCode Solutions/March 2021 LeetCoding Challenge/Short Encoding of Words/Short Encoding of Words.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minimumLengthEncoding(self, words: List[str]) -> int:
-        wordsSet = set(words)
-        for word in words:
-            if (word in wordsSet):
-                for i in range(1,len(word)):
-                    wordsSet.discard(word[i:])
-        return len("#".join(list(wordsSet))) + 1

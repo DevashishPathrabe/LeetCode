f2bbf5950fecc0abcbb26f518fb8281c2ae26314
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-12-06 10:14:13 +0530

    Delete Count Primes.py

--- LeetCode Solutions/May 2021 LeetCoding Challenge/Count Primes/Count Primes.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countPrimes(self, n: int) -> int:
-        arr, count = [0] * n, 0
-        for i in range(2, n):
-            if (arr[i]):
-                continue
-            count += 1
-            arr[i * i:n:i] = [1] * ((n-1) // i-i+1)
-        return count

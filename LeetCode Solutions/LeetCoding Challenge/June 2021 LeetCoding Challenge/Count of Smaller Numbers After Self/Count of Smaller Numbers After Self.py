5806bf6fed06a794a20d9d7d1a0b2834af9da2f1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-12-08 12:52:15 +0530

    Count of Smaller Numbers After Self

---
+++ LeetCode Solutions/LeetCoding Challenge/June 2021 LeetCoding Challenge/Count of Smaller Numbers After Self/Count of Smaller Numbers After Self.py
@@ -0,0 +1,12 @@
+import bisect
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        if (len(nums) < 1):
+            return []
+        arr = []
+        result = []
+        for num in nums[::-1]:
+            index = bisect.bisect_left(arr, num)
+            result.append(index)
+            arr.insert(index, num)
+        return result[::-1]

904be40294ae4c0484832e25b99ed942e63fccac
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-12-09 15:02:14 +0530

    Number of Matching Subsequences

---
+++ LeetCode Solutions/LeetCoding Challenge/June 2021 LeetCoding Challenge/Number of Matching Subsequences/Number of Matching Subsequences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        numberOfWords = 0
+        for word, freq in Counter(words).items():
+            index, match = 0, True
+            for char in word:
+                index = s.find(char, index) + 1
+                if (not index):
+                    match = False
+                    break
+            if (match):
+                numberOfWords += freq
+        return numberOfWords

a286e581b9ca23749a0ae52a812a2ba177e25572
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-12-13 22:20:02 +0530

    Hamming Distance

---
+++ LeetCode Solutions/LeetCoding Challenge/November 2021 LeetCoding Challenge/Hamming Distance.c
@@ -0,0 +1,13 @@
+
+
+int hammingDistance(int x, int y){
+    int hammingDistance = 0;
+    for(int i=0; i<32; i++){
+        if((x&1) != (y&1)){
+            hammingDistance++;
+        }
+        x = x>>1;
+        y = y>>1;
+    }
+    return hammingDistance;
+}
